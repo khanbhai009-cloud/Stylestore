c205a19bbfefe0b1b724261e00d69eeb8b87682e
khanbhai009-cloud <khanbhai9236716296@gmail.com>
2025-08-12 08:04:49 +0530

    Delete lib/supabaseConfig.ts

--- lib/supabaseConfig.ts
+++
@@ -1,8 +0,0 @@
-export const supabaseConfig = {
-  url: process.env.NEXT_PUBLIC_SUPABASE_URL,
-  anonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-};
-
-if (!supabaseConfig.url || !supabaseConfig.anonKey) {
-  throw new Error("Missing Supabase env variables!");
-}
